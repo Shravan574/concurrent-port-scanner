b39d3f6297e9804b6a9163474c73ab66706e201b
Malyala Sai Sravan <shravanmalyala66@gmail.com>
2026-06-20 00:23:52 +0530

    chore: seed main.go starter file

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"concurrent-port-scanner/pkg/helper"
+)
+
+func main() {
+	fmt.Println("Starting complex Go application concurrent-port-scanner...")
+	fmt.Println(helper.Greet("Developer"))
+}
